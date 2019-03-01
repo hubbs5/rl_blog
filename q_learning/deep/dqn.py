b581ebc7c8c5c8d13ae62e13f8c8f551ab8e725a
Christian <christiandhubbs@gmail.com>
2019-02-28 20:19:59 -0600

    adding dqn algorithm for openai gym

---
+++ q_learning/deep/dqn.py
@@ -0,0 +1,378 @@
+#!/usr/bin/env python
+
+# DQN for playing OpenAI Atari RAM. For full writeup, visit:
+# https://www.datahubbs.com
+
+import numpy as np
+import sys
+import os
+from argparse import ArgumentParser
+import matplotlib.pyplot as plt
+import gym
+import torch
+from torch import nn
+from collections import namedtuple, deque, OrderedDict
+from copy import deepcopy
+import time
+import shutil
+
+def main(argv):
+
+    args = parse_arguments()
+    if args.gpu is None or args.gpu == False:
+        args.gpu = 'cpu'
+    else:
+        args.gpu = 'cuda'
+
+    # Initialize environment
+    env = gym.make(args.env)
+
+    # Initialize DQNetwork
+    dqn = QNetwork(env=env, 
+        n_hidden_layers=args.hl, 
+        n_hidden_nodes=args.hn, 
+        learning_rate=args.lr, 
+        bias=args.bias,
+        device=args.gpu)
+    print(dqn)
+    # Initialize DQNAgent
+    agent = DQNAgent(env, dqn,  
+        memory_size=args.memorySize,
+        burn_in=args.burnIn,
+        path=args.path) 
+        # epsilon_start=args.epsStart,
+        # epsilon_end=args.epsEnd,
+        # epsilon_strategy=args.epsStrategy,
+        # epsilon=args.epsConstant, 
+        #window=args.window)
+
+    # Train agent
+    start_time = time.time()
+    agent.train(epsilon=args.epsStart,
+        gamma=args.gamma, 
+        max_episodes=args.maxEps,
+        batch_size=args.batch,
+        update_freq = args.updateFreq,
+        network_sync_frequency=args.netSyncFreq)
+    end_time = time.time()
+    # Save results
+    if agent.success:
+        agent.save_results(args)
+        if args.plot:
+            agent.plot_rewards()
+    else:
+        shutil.rmtree(agent.path)
+
+    x = end_time - start_time
+    hours, remainder = divmod(x, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    print("Agent training: {}".format(agent.success))
+    print("Peak mean reward: {:.2f}".format(
+        max(agent.mean_training_rewards)))
+    print("Training Time: {:02}:{:02}:{:02}\n".format(
+        int(hours), int(minutes), int(seconds)))
+
+
+class DQNAgent:
+    
+    def __init__(self, env, network, memory_size=50000,
+        batch_size=32, burn_in=10000, path=None, *args, **kwargs):
+        
+        self.env = env
+        self.env_name = env.spec.id
+        self.network = network
+        self.target_network = deepcopy(network)
+        self.batch_size = batch_size
+        self.window = 100
+        self.reward_threshold = 300 if 'CartPole' not in self.env_name \
+        	else 195
+        self.path = path
+        self.timestamp = time.strftime('%Y%m%d_%H%M')
+        self.initialize(memory_size, burn_in)
+    
+    def take_step(self, mode='train'):
+        if mode == 'explore':
+            action = self.env.action_space.sample()
+        else:
+            action = self.network.get_action(self.s_0, epsilon=self.epsilon)
+            self.step_count += 1
+        s_1, r, done, _ = self.env.step(action)
+        self.rewards += r
+        self.buffer.append(self.s_0, action, r, done, s_1)
+        self.s_0 = s_1
+        if done:
+            self.s_0 = self.env.reset()
+        return done
+        
+    # Implement DQN training algorithm
+    def train(self, epsilon=0.05, gamma=0.99, max_episodes=10000,
+        batch_size=32, network_sync_frequency=5000, update_freq=4):
+        self.gamma = gamma
+        self.epsilon = epsilon
+        # Populate replay buffer
+        while self.buffer.burn_in_capacity() < 1:
+            self.take_step(mode='explore')
+            
+        ep = 0
+        training = True
+        while training:
+            self.s_0 = self.env.reset()
+            self.rewards = 0
+            done = False
+            while done == False:
+                done = self.take_step(mode='train')
+                # Update network
+                if self.step_count % update_freq == 0:
+                	self.update()
+	            # Sync networks
+                if self.step_count % network_sync_frequency == 0:
+                    self.target_network.load_state_dict(
+                        self.network.state_dict())
+                    
+                if done:
+                    ep += 1
+                    self.training_rewards.append(self.rewards)
+                    self.training_loss.append(np.mean(self.update_loss))
+                    self.update_loss = []
+                    mean_rewards = np.mean(
+                        self.training_rewards[-self.window:])
+                    self.mean_training_rewards.append(mean_rewards)
+                    print("\rEpisode {:d} Mean Rewards {:.2f}\t\t".format(
+                        ep, mean_rewards), end="")
+                    
+                    if ep >= max_episodes:
+                        training = False
+                        print('\nEpisode limit reached.')
+                        break
+                    if mean_rewards >= self.reward_threshold:
+                        training = False
+                        print('Environment solved in {} episodes!'.format(
+                            self.step_count))
+                        break
+                        
+    def calculate_loss(self, batch):
+        states, actions, rewards, dones, next_states = [i for i in batch]
+        rewards_t = torch.FloatTensor(rewards).to(device=self.network.device)
+        actions_t = torch.LongTensor(np.array(actions)).reshape(-1,1).to(
+            device=self.network.device)
+        dones_t = torch.ByteTensor(dones).to(device=self.network.device)
+        
+        qvals = torch.gather(self.network.get_qvals(states), 1, actions_t)
+        qvals_next = torch.max(self.target_network.get_qvals(next_states),
+                               dim=-1)[0].detach()
+        qvals_next[dones_t] = 0 # Zero-out terminal states
+        expected_qvals = self.gamma * qvals_next + rewards_t
+        loss = nn.MSELoss()(qvals, expected_qvals.reshape(-1,1))
+        return loss
+    
+    def update(self):
+        self.network.optimizer.zero_grad()
+        batch = self.buffer.sample_batch(batch_size=self.batch_size)
+        loss = self.calculate_loss(batch)
+        loss.backward()
+        self.network.optimizer.step()
+        if self.network.device == 'cuda':
+            self.update_loss.append(loss.detach().cpu().numpy())
+        else:
+            self.update_loss.append(loss.detach().numpy())
+        
+    def initialize(self, memory_size, burn_in):
+        self.buffer = experienceReplayBuffer(memory_size, burn_in)
+        self.training_rewards = []
+        self.training_loss = []
+        self.update_loss = []
+        self.mean_training_rewards = []
+        self.rewards = 0
+        self.s_0 = self.env.reset()
+        self.step_count = 0
+        self.success = False
+        if self.path is None:
+            self.path = os.path.join(os.getcwd(), 
+            	self.env_name, self.timestamp)
+        os.makedirs(self.path, exist_ok=True)
+
+    def plot_rewards(self):
+        plt.figure(figsize=(12,8))
+        plt.plot(self.training_rewards, label='Rewards')
+        plt.plot(self.mean_training_rewards, label='Mean Rewards')
+        plt.xlabel('Episodes')
+        plt.ylabel('Rewards')
+        plt.ylim([0, np.round(self.reward_threshold)*1.05])
+        plt.savefig(os.path.join(self.path, 'rewards.png'))
+        plt.show()
+
+    def save_results(self, args):
+        weights_path = os.path.join(self.path, 'dqn_weights.pt')
+        torch.save(self.network.stat_dict(), weights_path)
+        # Save rewards
+        rewards = pd.DataFrame(self.training_rewards, columns=['reward'])
+        rewards.insert(0, 'episode', rewards.index.values)
+        rewards.to_csv(os.path.join(self.path, 'rewards.txt'))
+        # Save model parameters
+        file = open(os.path.join(self.path, 'parameters.txt'), 'w')
+        file.writelines('rewards')
+        [file.writelines('\n' + str(k) + ',' + str(v)) 
+            for k, v in vars(args).items()]
+        file.close()
+
+class QNetwork(nn.Module):
+    
+    def __init__(self, env, learning_rate=1e-3, n_hidden_layers=4,
+        n_hidden_nodes=256, bias=True, activation_function='relu',
+        device='cpu', *args, **kwargs):
+        super(QNetwork, self).__init__()
+        self.device = device
+        self.actions = np.arange(env.action_space.n)
+        n_inputs = env.observation_space.shape[0]
+        n_outputs = env.action_space.n
+
+        activation_function = activation_function.lower()
+        if activation_function == 'relu':
+            act_func = nn.ReLU()
+        elif activation_function == 'tanh':
+            act_func = nn.Tanh()
+        elif activation_function == 'elu':
+            act_func = nn.ELU()
+        elif activation_function == 'sigmoid':
+            act_func = nn.Sigmoid()
+        elif activation_function == 'selu':
+            act_func = nn.SELU()
+        
+        # Build a network dependent on the hidden layer and node parameters
+        layers = OrderedDict()
+        n_layers = 2 * (n_hidden_layers-1)
+        for i in range(n_layers + 1):
+            if n_hidden_layers == 0:
+                layers[str(i)] = nn.Linear(
+                    n_inputs,
+                    n_outputs,
+                    bias=bias)
+            elif i == n_layers:
+                layers[str(i)] = nn.Linear(
+                    n_hidden_nodes,
+                    n_outputs,
+                    bias=bias)
+            elif i % 2 == 0 and i == 0:
+                layers[str(i)] = nn.Linear(
+                    n_inputs,
+                    n_hidden_nodes,
+                    bias=bias)
+            elif i % 2 == 0 and i < n_layers - 1:
+                layers[str(i)] = nn.Linear(
+                    n_hidden_nodes,
+                    n_hidden_nodes,
+                    bias=bias)
+            else:
+                layers[str(i)] = act_func
+                
+        self.network = nn.Sequential(layers)
+        
+        # Set device for GPU's
+        if self.device == 'cuda':
+            self.network.cuda()
+        
+        self.optimizer = torch.optim.Adam(self.parameters(),
+                                          lr=learning_rate)
+                
+    def get_action(self, state, epsilon=0.05):
+        if np.random.random() < epsilon:
+            action = np.random.choice(self.actions)
+        else:
+            action = self.greedy_action(state)
+        return action
+    
+    def greedy_action(self, state):
+        qvals = self.get_qvals(state)
+        return torch.max(qvals, dim=-1)[1].item()
+    
+    def get_qvals(self, state):
+        state_t = torch.FloatTensor(state).to(device=self.device)
+        return self.network(state_t)
+
+class experienceReplayBuffer:
+
+    def __init__(self, memory_size=50000, burn_in=10000):
+        self.memory_size = memory_size
+        self.burn_in = burn_in
+        self.Buffer = namedtuple('Buffer', 
+            field_names=['state', 'action', 'reward', 'done', 'next_state'])
+        self.replayMemory = deque(maxlen=memory_size)
+
+    def sample_batch(self, batch_size=32):
+        samples = np.random.choice(len(self.replayMemory), batch_size, 
+            replace=False)
+        # Use asterisk operator to unpack deque 
+        batch = zip(*[self.replayMemory[i] for i in samples])
+        return batch
+
+    def append(self, state, action, reward, done, next_state):
+        self.replayMemory.append(
+            self.Buffer(state, action, reward, done, next_state))
+
+    def burn_in_capacity(self):
+        return len(self.replayMemory) / self.burn_in
+    
+    def capacity(self):
+        return len(self.replayMemory) / self.memory_size
+
+def parse_arguments():
+    parser = ArgumentParser(description='Deep Q Network Argument Parser')
+    # Network parameters
+    parser.add_argument('--hl', type=int, default=4,
+        help='An integer number that defines the number of hidden layers.')
+    parser.add_argument('--hn', type=int, default=256,
+        help='An integer number that defines the number of hidden nodes.')
+    parser.add_argument('--lr', type=float, default=25e-5,
+        help='An integer number that defines the number of hidden layers.')
+    parser.add_argument('--bias', type=str2bool, default=True,
+        help='Boolean to determine whether or not to use biases in network.')
+    parser.add_argument('--actFunc', type=str, default='relu',
+        help='Set activation function.')
+    parser.add_argument('--gpu', type=str2bool, default=False,
+        help='Boolean to enable GPU computation. Default set to False.')
+    parser.add_argument('--env', dest='env', type=str, default='CartPole-v0')
+    parser.add_argument('--gamma', type=float, default=0.99,
+        help='A value between 0 and 1 to discount future rewards.')
+    parser.add_argument('--maxEps', type=int, default=10000,
+        help='An integer number of episodes to train the agent on.')
+    parser.add_argument('--netSyncFreq', type=int, default=10000,
+        help='An integer number that defines steps to update the target network.')
+    parser.add_argument('--updateFreq', type=int, default=4,
+        help='Integer value that determines how many steps or episodes' + 
+        'must be completed before a backpropogation update is taken.')
+    parser.add_argument('--batch', type=int, default=32,
+        help='An integer number that defines the batch size.')
+    parser.add_argument('--memorySize', type=int, default=50000,
+        help='An integer number that defines the replay buffer size.')
+    parser.add_argument('--burnIn', type=int, default=10000,
+        help='Set the number of random burn-in transitions before training.')
+    parser.add_argument('--epsStart', type=float, default=0.05,
+        help='Float value for the start of the epsilon decay.')
+    parser.add_argument('--epsEnd', type=float, default=0.01,
+        help='Float value for the end of the epsilon decay.')
+    parser.add_argument('--epsStrategy', type=str, default='constant',
+        help="Enter 'constant' to set epsilon to a constant value or 'decay'" + 
+        "to have the value decay over time. If 'decay', ensure proper" + 
+        "start and end values.")
+    parser.add_argument('--epsConstant', type=float, default=0.05,
+        help='Float to be used in conjunction with a constant epsilon strategy.')
+    parser.add_argument('--window', type=int, default=100,
+        help='Integer value to set the moving average window.')
+    parser.add_argument('--plot', type=str2bool, default=True,
+        help='If true, plot training results.')
+    parser.add_argument('--path', type=str, default=None,
+    	help='Specify path to save results.')
+    args = parser.parse_args()
+
+    return parser.parse_args()
+
+def str2bool(argument):
+    if argument.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif argument.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False 
+    else:
+        raise ArgumentTypeError('Boolean value expected.')
+
+if __name__ == '__main__':
+    main(sys.argv)
